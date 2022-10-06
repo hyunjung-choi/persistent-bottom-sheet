28aca97c939c263d7f69a830a8f595a71f16ebe5
최현정 <hjchoi.dev@gmail.com>
2022-10-06 22:18:21 +0900

    :sparkles: BottomSheetBehavior 속성에 따른 Log와 Toast 추가

--- app/src/main/java/com/hyunjung/persistentbottomsheet/MainActivity.kt
+++ app/src/main/java/com/hyunjung/persistentbottomsheet/MainActivity.kt
@@ -2,10 +2,14 @@ package com.hyunjung.persistentbottomsheet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.hyunjung.persistentbottomsheet.databinding.ActivityMainBinding
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
 
@@ -35,10 +39,18 @@ class MainActivity : AppCompatActivity() {
         behavior = BottomSheetBehavior.from(binding.persistentBottomSheet)
         behavior.addBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
             override fun onStateChanged(bottomSheet: View, newState: Int) {
+                when(newState) {
+                    BottomSheetBehavior.STATE_COLLAPSED -> Log.d(TAG, "onStateChanged: COLLAPSED")
+                    BottomSheetBehavior.STATE_DRAGGING -> Log.d(TAG, "onStateChanged: DRAGGING")
+                    BottomSheetBehavior.STATE_EXPANDED -> Log.d(TAG, "onStateChanged: EXPANDED")
+                    BottomSheetBehavior.STATE_HIDDEN -> Log.d(TAG, "onStateChanged: HIDDEN")
+                    BottomSheetBehavior.STATE_SETTLING -> Log.d(TAG, "onStateChanged: SETTING")
+                }
             }
 
             // 슬라이드 되는 도중 계속 호출
             override fun onSlide(bottomSheet: View, slideOffset: Float) {
+                Toast.makeText(this@MainActivity, "onSlide", Toast.LENGTH_SHORT).show()
             }
         })
 
