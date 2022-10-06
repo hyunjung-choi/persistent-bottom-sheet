2810a3d2520f12769caed99be13427d24713d812
최현정 <hjchoi.dev@gmail.com>
2022-10-06 21:49:49 +0900

    :sparkles: Add Button Click Event

--- app/src/main/java/com/hyunjung/persistentbottomsheet/MainActivity.kt
+++ app/src/main/java/com/hyunjung/persistentbottomsheet/MainActivity.kt
@@ -2,15 +2,45 @@ package com.hyunjung.persistentbottomsheet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.hyunjung.persistentbottomsheet.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
+    lateinit var behavior: BottomSheetBehavior<ConstraintLayout>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        initEvent()
+    }
+
+    private fun initEvent() {
+        persistentBottomSheetEvent()
+
+        binding.btnExpanded.setOnClickListener {
+            behavior.state = BottomSheetBehavior.STATE_EXPANDED
+        }
+
+        binding.btnCollapsed.setOnClickListener {
+            behavior.state = BottomSheetBehavior.STATE_COLLAPSED
+        }
+    }
+
+    private fun persistentBottomSheetEvent() {
+        behavior = BottomSheetBehavior.from(binding.persistentBottomSheet)
+        behavior.addBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+            }
+
+            // 슬라이드 되는 도중 계속 호출
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+            }
+        })
+
     }
 }
